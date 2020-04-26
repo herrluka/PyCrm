c440e63c8107a0fc2320bbb52b8ca6ab545b6c1e
Luka Radujević <luka.radujevic@gmail.com>
2020-04-26 14:37:35 +0200

    Na brencu

--- Run.py
+++ Run.py
@@ -1,4 +1,7 @@
 from Student import Student
-
+from Worker import Worker
 s = Student("Lore")
 s.learn()
+
+worker = Worker("luka", "raduj")
+worker.work()
---
+++ Worker.py
@@ -0,0 +1,8 @@
+class Worker:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def work(self):
+        print("arbeit!!")
